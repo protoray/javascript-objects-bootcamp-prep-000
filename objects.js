3439bc21016b5571d7c57b01c9d59cc40fbe4ac6
Ray <ray@protozo.net>
2018-02-17 07:32:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,12 +9,12 @@ function updatePlaylist(playlist, artistName, songTitle)
 {
   
   playlist[`${artistName}`] = songTitle;
-  return playlist
+  return playlist;
   
 }
 
 function removeFromPlaylist(playlist, artistName)
 {
   delete playlist.artistName;
-  
+  return playlist;
 }
